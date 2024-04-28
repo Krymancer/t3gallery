208e69bf77596c72e5e961159b491aaf78c50ed6
Júnior Nascimento <junior.nascm@gmail.com>
2024-04-28 15:38:29 -0300

    chore: setting up shadcn-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import "~/styles/globals.css";
+import { Inter as FontSans } from "next/font/google"
 
-import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({
+import { cn } from "~/lib/utils"
+import { Toaster } from "~/components/ui/toaster"
+
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-});
+})
 
 export const metadata = {
   title: "T3 Gallery",
@@ -21,8 +24,16 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+        <Toaster />
       </html>
     </ClerkProvider>
   );
